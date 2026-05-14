c3f9daf31e675b3f2bd67ec2eeebfc0d87b99e10
Faiz Ahmad <faizahmad10102003@gmail.com>
2026-05-14 22:47:32 +0500

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-function App() {
-  return (
-    <div className="h-screen bg-black text-white flex items-center justify-center text-5xl font-bold">
-      Stay Space 🚀
-    </div>
-  );
-}
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from  './pages/About'
+import Profile from './pages/Profile'
 
-export default App
+export default function App() {
+  return <BrowserRouter>
+  <Routes>
+   
+   <Route path ="/" element={ <Home />} />
+   <Route path ="/sign-in" element={ <SignIn />} />
+   <Route path ="/sign-up" element={ <SignUp />} />
+   <Route path ="/about" element={ <About />} />
+   <Route path ="/profile" element={ <Profile />} />
+
+
+
+  </Routes>
+  </BrowserRouter>
+}
